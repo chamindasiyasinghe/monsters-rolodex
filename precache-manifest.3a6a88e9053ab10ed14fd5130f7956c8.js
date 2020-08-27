2a2516440e35b95aee992ef6e6368f44c519a9fd
Chaminda Siyasinghe <chamindasc@yahoo.com>
2020-08-28 09:49:18 +1200

    Updates

---
+++ precache-manifest.3a6a88e9053ab10ed14fd5130f7956c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecafbd9453feaa140ac0ad7ac5df6bcc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f7512b88fc9948059123",
+    "url": "/monsters-rolodex/static/css/main.0a6c3132.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7512b88fc9948059123",
+    "url": "/monsters-rolodex/static/js/main.52239b88.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
